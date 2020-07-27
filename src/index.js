84495cbf4297c706e2b44ae68180f94bf84b55ff
Devon Navon <devon.navon@gmail.com>
2020-07-27 11:27:21 -0700

    initial apollo graphql setup

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+app.use(cors());
+
+const schema = gql`
+	type Query {
+		users: [User!]
+		me: User
+		user(id: ID!): User
+	}
+
+	type User {
+		id: ID!
+		username: String!
+	}
+`;
+
+let users = {
+	1: {
+		id: '1',
+		username: 'Robin Wieruch',
+	},
+	2: {
+		id: '2',
+		username: 'Dave Davids',
+	},
+};
+
+const me = users[1];
+
+const resolvers = {
+	Query: {
+		users: () => {
+			return Object.values(users);
+		},
+		user: (parent, { id }) => {
+			return users[id];
+		},
+		me: (parent, args, { me }) => {
+			return me;
+		},
+	},
+	// User: {
+	// 	username: (user) => `${user.firstname} ${user.lastname}`,
+	// },
+};
+
+const server = new ApolloServer({
+	typeDefs: schema,
+	resolvers,
+	context: {
+		me: users[1],
+	},
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+	console.log('Apollo Server on http://localhost:8000/graphql');
+});

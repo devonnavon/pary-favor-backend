f2910be3c441cbcf09b7aa5a6789470f4ff0c756
Devon Navon <devon.navon@gmail.com>
2020-07-29 11:27:19 -0700

    testing added loaders ot improve query performance also done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import DataLoader from 'dataloader';
 import { ApolloServer, AuthenticationError } from 'apollo-server-express';
 import http from 'http'; //for subscription setup
 
 import schema from './schema';
 import resolvers from './resolvers';
 import models, { sequelize } from './models';
+import loaders from './loaders';
 
 const app = express();
 
@@ -42,7 +44,12 @@ const server = new ApolloServer({
 	},
 	context: async ({ req, connection }) => {
 		if (connection) {
-			return { models };
+			return {
+				models,
+				loaders: {
+					user: new DataLoader((keys) => loaders.user.batchUsers(keys, models)),
+				},
+			};
 		}
 
 		if (req) {
@@ -51,6 +58,9 @@ const server = new ApolloServer({
 				models,
 				me,
 				secret: process.env.SECRET,
+				loaders: {
+					user: new DataLoader((keys) => loaders.user.batchUsers(keys, models)),
+				},
 			};
 		}
 	},
@@ -61,10 +71,15 @@ server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
-const eraseDatabaseOnSync = true;
+// const eraseDatabaseOnSync = true;
 
-sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
-	if (eraseDatabaseOnSync) {
+// sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+
+const isTest = !!process.env.TEST_DATABASE;
+
+sequelize.sync({ force: isTest }).then(async () => {
+	if (isTest) {
+		// if (eraseDatabaseOnSync) {
 		createUsersWithMessages(new Date());
 	}
 
@@ -80,6 +95,7 @@ const createUsersWithMessages = async (date) => {
 			username: 'len',
 			email: 'len@noshi.globe',
 			password: 'qqqqqqq',
+			role: 'ADMIN',
 			messages: [
 				{
 					text: 'yo yo yo',

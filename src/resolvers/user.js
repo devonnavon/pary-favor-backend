ee020ae8a68a5cc48bd76662892aa54c6c1684b2
Devon Navon <devon.navon@gmail.com>
2020-08-14 11:49:31 -0700

    increased token expiration to 2hrs for login and signup

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -39,7 +39,7 @@ export default {
 				password,
 			});
 
-			return { token: createToken(user, secret, '1hr') };
+			return { token: createToken(user, secret, '2hr') };
 		},
 		signIn: async (parent, { login, password }, { models, secret }) => {
 			const user = await models.User.findByLogin(login);
@@ -54,7 +54,7 @@ export default {
 				throw new AuthenticationError('Invalid password.');
 			}
 
-			return { token: createToken(user, secret, '30m') };
+			return { token: createToken(user, secret, '2hr') };
 		},
 		deleteUser: combineResolvers(
 			isAdmin,

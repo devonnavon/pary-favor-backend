351c73bcb00b9013be0144c213418f7e1ddfa80e
Devon Navon <devon.navon@gmail.com>
2020-09-01 08:32:55 -0700

    removed options and sorOrder fields, not used with grid system

--- src/models/cardItem.js
+++ src/models/cardItem.js
@@ -10,10 +10,6 @@ const cardItem = (sequelize, DataTypes) => {
 				},
 			},
 		},
-		options: {
-			type: DataTypes.JSON,
-			allowNull: true,
-		},
 		url: {
 			type: DataTypes.STRING,
 			allowNull: true,
@@ -21,10 +17,6 @@ const cardItem = (sequelize, DataTypes) => {
 		text: {
 			type: DataTypes.TEXT,
 			allowNull: true,
-		},
-		sortOrder: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
 		},
 	});
 
